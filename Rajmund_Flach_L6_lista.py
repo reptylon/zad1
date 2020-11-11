f3ff66523c545087984a378ba619257b89c1beae
reptylon <reptylon257@gmail.com>
2020-11-11 16:57:03 +0100

    zadanie

--- Rajmund_Flach_L6_lista.py
+++ Rajmund_Flach_L6_lista.py
@@ -1,2 +1,3 @@
-print('hello')
-print('hello')
+list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1]]
+print('Lista przed posortowaniem: ', list_to_sort)
+print('Lista po posortowaniu: ', sorted(list_to_sort, key=lambda x: (x[1], x[2])))
